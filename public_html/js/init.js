662582b4eb9b3c12eae86e49667401151f10a7bb
Jason Tan <jastan313@gmail.com>
2017-10-11 16:31:56 -0700

    Added Facebook login, AJAX calls

--- public_html/js/init.js
+++ public_html/js/init.js
@@ -7,47 +7,48 @@
 
 // Check if there is jQuery
 if (typeof jQuery === 'undefined') {
-  throw new Error('Bootstrap\'s JavaScript requires jQuery')
+    throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
-$(function() {
-    
-    // Validation functions for user input
-    var validateName = function (name) {
-        return name.match(/^[A-Za-z]*\s[A-Za-z]+$/);
-    }
-    var validateEmail = function (email) {
-        return email.match(/^[A-Za-z\._\-0-9]+[@][A-Za-z]+[\.][a-z]{2,4}$/);
-    };
-    var validatePassword = function (password) {
-        return password.length >= 6;
-    }
-    
+
+// Validation functions for user input
+function validateName(name) {
+    return name.match(/^[A-Za-z]*\s[A-Za-za]+$/);
+}
+function validateEmail(email) {
+    return email.match(/^[A-Za-z\._\-0-9]+[@][A-Za-z]+[\.][a-z]{2,4}$/);
+}
+;
+function validatePassword(password) {
+    return password.length >= 6;
+}
+
+$(function () {
+    // Navigation UI setup
     $('#signup-btn').click(function () {
         $('#login-container').hide();
         $('#signup-container').show();
     });
-    
-    $('#back-btn').click(function() {
-       $('#login-container').show();
-       $('#signup-container').hide();
+    $('#back-btn').click(function () {
+        $('#login-container').show();
+        $('#signup-container').hide();
     });
-    
+
     // User login
     $('#login-form').submit(function () {
-        
+
         // Hide previous errors
         $('#alert-login-email-invalid').hide();
         $('#alert-login-email-mismatch').hide();
         $('#alert-login-password-invalid').hide();
         $('#alert-login-password-mismatch').hide();
-        
+
         var email = $('#login-email').val();
         var password = $('#login-password').val();
-        
+
         // Validate user inputs and show error if not valid
         var err_flag = false;
-        if(!validateEmail(email)) {
+        if (!validateEmail(email)) {
             $('#alert-login-email-invalid').show();
             err_flag = true;
         }
@@ -55,29 +56,51 @@ $(function() {
             $('#alert-login-password-invalid').show();
             err_flag = true;
         }
-        
+
         // If validation successful, attempt GET user account
-        if(!err_flag) {
-            // Put AJAX call here
-            alert('SUCCESS: Email: ' + email + ", Password: " + password);
+        if (!err_flag) {
+            console.log('Validation Login Success: Email: ' + email + ", Password: " + password);
+            var user = {"email": email, "password": password};
+            var data = JSON.stringify(user);
+            $.ajax({
+                type: 'GET',
+                url: 'get_demo',
+                data: data,
+                success: function (request) {
+                    console.log('GET Success Response: ' + request.result);
+                    console.log('GET Email Not Exist Result: ' + request.email_red);
+                    console.log('GET Password Incorrect Result: ' + request.password_red);
+                    console.log('GET Success Result: ' + request.green);
+                },
+                error: function (request, status, error) {
+                    console.log('GET Error Response: ' + request.responseText);
+                    console.log('GET Status: ' + status);
+                    console.log('GET Error: ' + error);
+                },
+                contentType: "application/json",
+                dataType: 'json'
+            });
         }
         return false;
     });
-    
-    // User signup
-    $('#signup-form').submit(function () {
-        
+});
+
+
+// Checks Facebook login status. Signs up user if logged in. Facebook login if otherwise.
+function fbStatusChangeCallback(response) {
+    // User is Facebook logged in
+    if (response.status == 'connected') {
+
         // Hide previous errors
         $('#alert-signup-name-invalid').hide();
         $('#alert-signup-email-invalid').hide();
         $('#alert-signup-email-taken').hide();
         $('#alert-signup-password-invalid').hide();
         $('#alert-signup-connect-invalid').hide();
-        
+
         var name = $('#signup-name').val();
         var email = $('#signup-email').val();
         var password = $('#signup-password').val();
-        var connect = $('#signup-connect').prop('checked');
 
         // Validate user inputs and show error if not valid
         var err_flag = false;
@@ -93,16 +116,45 @@ $(function() {
             $('#alert-signup-password-invalid').show();
             err_flag = true;
         }
-        if(!connect) {
-             $('#alert-signup-connect-invalid').show();
-             err_flag = true;
-        }
-        
-        // If validation successful, attempt POST new user account
+
+        // If validation successful, POST new user account.
         if (!err_flag) {
-            // Put AJAX call here
-            alert('SUCCESS: Name: ' + name + ", Email: " + email + ", Password: " + password + ", Connect: " + connect);
+            console.log('Validation Signup Success: Name: ' + name + ', Email: ' + email + ", Password: " + password);
+            var user = {"name": name, "email": email, "password": password};
+            var data = JSON.stringify(user);
+            $.ajax({
+                type: 'POST',
+                url: 'post_demo',
+                data: data,
+                success: function (request) {
+                    console.log('POST Success Response: ' + request.result);
+                    console.log('POST Email Taken Result: ' + request.email_red);
+                    console.log('POST Success Result: ' + request.green);
+                },
+                error: function (request, status, error) {
+                    console.log('POST Error Response: ' + request.responseText);
+                    console.log('POST Status: ' + status);
+                    console.log('POST Error: ' + error);
+                },
+                contentType: "application/json",
+                dataType: 'json'
+            });
         }
-        return false;
+    } 
+    // User is not Facebook logged in
+    else {
+        FB.login(function (response) {
+            fbStatusChangeCallback(response);
+        });
+    }
+}
+;
+
+// User signup
+function signupSubmit() {
+    FB.getLoginStatus(function (response) {
+        fbStatusChangeCallback(response);
     });
-});
+    return false;
+}
+;
